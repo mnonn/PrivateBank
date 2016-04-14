c4fb1164db25b7edb4539c44582bb95b84bf952f
mnonn <blumenkind2005@gmail.com>
2016-04-14 14:05:31 +0200

    updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'PrivateBankApp'
+set :application, 'PrivateBank'
 set :repo_url, 'git@github.com:mnonn/PrivateBank.git'
 
 # Default branch is :master
