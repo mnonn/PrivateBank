fff1ca896c9b31da51f19a0abb117262928816bc
mnonn <blumenkind@uni-koblenz.de>
2016-04-21 11:35:43 +0200

    mailer updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,10 +69,11 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: "gmail.com",
-    authentication: "plain",
+    authentication: "login",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    password: ENV["GMAIL_PASSWORD"],
+    openssl_verify_mode: "none"
   }
   config.action_mailer.perform_deliveries= true
 
